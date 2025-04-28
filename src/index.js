7d2c760ae3f3b4bc687a89971e99b92c8c1516f3
kartikayy-01 <kartikayb1405@gmail.com>
2025-04-28 11:37:27 +0530

    custom api,user & video model created,cloudinary setup

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ dotenv.config({
     path:"./env"
 })
 
-connectDB() 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`server is running at ${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log(`MONGO DB connection failed: ${error}`);
+    
+})
