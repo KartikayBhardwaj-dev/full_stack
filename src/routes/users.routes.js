2346ba09cdc371ba8cec6bc306b2a034813a5a66
kartikayy-01 <kartikayb1405@gmail.com>
2025-05-06 12:30:06 +0530

    added routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,13 @@ import { upload } from "../middleware/multer.middleware.js"
 import { 
     registerUser,
     loginUser,
-    logoutUser 
+    logoutUser, 
+    updateCurrentPassword,
+    getCurrentUser,
+    updateCurrentDetails,
+    updateAvatarFile,
+    updateCoverImageFile,
+    getWatchHistory
 } from "../controllers/users.controllers.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
@@ -27,4 +33,11 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/change-password").post(verifyJWT,updateCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-details").patch(verifyJWT,updateCurrentDetails)
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateAvatarFile)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImageFile)
+router.route("/c/:username").get(verifyJWT, getCurrentUser)
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router 
